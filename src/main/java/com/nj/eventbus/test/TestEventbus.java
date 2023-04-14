20bbfdb74bddcb05895efbbada9d918298f121e5
ForDream <122186126@qq.com>
2023-04-14 17:15:13 +0800

    test

--- src/main/java/com/nj/eventbus/test/TestEventbus.java
+++ src/main/java/com/nj/eventbus/test/TestEventbus.java
@@ -5,7 +5,9 @@ import com.nj.eventbus.StreamEventbus;
 public class TestEventbus {
 
     public static void main(String[] args) {
-        testParallel();
+        // testSequential();
+        // ignoreException();
+        stopWhenErr();
     }
 
     public static void testParallel() {
@@ -13,6 +15,57 @@ public class TestEventbus {
                 // .sequential()
                 .parallel(10)
                 .ignoreException()
+                .post(Action0.class, "action0 with err")
+                .post(Action1.class, new String[] { "str0", "str1" })
+                .post(Action1.class, "action11")
+                .post(Action2.class, "action2 no err")
+                .post(Action3.class, 100_000)
+                .post(Action4.class, 4)
+                .exception((cause, info) -> {
+                    System.out.println(info);
+                    cause.printStackTrace();
+                })
+                .exec();
+    }
+
+    public static void testSequential() {
+        StreamEventbus.of("parallel")
+                .sequential()
+                .post(Action0.class, "action0 with err")
+                .post(Action1.class, new String[] { "str0", "str1" })
+                .post(Action1.class, "action11")
+                .post(Action2.class, "action2 no err")
+                .post(Action3.class, 100_000)
+                .post(Action4.class, 4)
+                .exception((cause, info) -> {
+                    System.out.println(info);
+                    cause.printStackTrace();
+                })
+                .exec();
+    }
+
+
+    
+    public static void ignoreException() {
+        StreamEventbus.of("parallel")
+                .sequential()
+                .ignoreException()
+                .post(Action0.class, "action0 with err")
+                .post(Action1.class, new String[] { "str0", "str1" })
+                .post(Action1.class, "action11")
+                .post(Action2.class, "action2 no err")
+                .post(Action3.class, 100_000)
+                .post(Action4.class, 4)
+                .exception((cause, info) -> {
+                    System.out.println(info);
+                    cause.printStackTrace();
+                })
+                .exec();
+    }
+    
+    public static void stopWhenErr() {
+        StreamEventbus.of("parallel")
+                .sequential()
                 .post(Action0.class, "action0 with err")
                 .post(Action1.class, new String[] { "str0", "str1" })
                 .post(Action1.class, "action11")
